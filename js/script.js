2fd2f439dbd4b7b31610472f4f3837905245e6d5
srh0820 <srh96820@gmail.com>
2024-08-04 03:54:41 +0900

    hd , main 정렬 및 스타일 수정중

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const header = document.getElementById('hd');
+    
+//     window.addEventListener('scroll', function () {
+//         if (window.scrollY > 0) {
+//             header.classList.add('scrolled');
+//         } else {
+//             header.classList.remove('scrolled');
+//         }
+//     });
+// });
